acf6cd5f8c3e9c1ca7616cb44c146ab8e0d73fe8
Yuriy Koval <yuriy.koval@phenompeople.com>
2023-08-31 19:12:11 +0300

    - filter existing data-space from add list;

--- portalkit-admin/libs/feature-account/src/lib/account-overlay/account-dataspace-dialog/account-data-space-dialog.component.ts
+++ portalkit-admin/libs/feature-account/src/lib/account-overlay/account-dataspace-dialog/account-data-space-dialog.component.ts
@@ -4,7 +4,7 @@ import {UntypedFormBuilder} from "@angular/forms";
 import {AccountService} from "../../account-page/account-page-data/account.service";
 import {NzNotificationService} from "ng-zorro-antd/notification";
 import {Account, AccountDataSpace, AttributeType, ValueName} from "../../account-page/account-page-data/account.types";
-import {Observable} from "rxjs";
+import {map, Observable, tap} from "rxjs";
 import {CachedAccountTypesService} from "../../account-page/account-page-data/cached-account-types.service";
 
 @Component({
@@ -16,6 +16,7 @@ export class AccountDataSpaceDialogComponent {
   account: Account;
   dataSpaces: Array<AccountDataSpace>;
   dataSpaceOptions$!: Observable<Array<ValueName>>;
+  canAddDataSpace = false;
 
   constructor(
     @Inject(NZ_MODAL_DATA) public modalData: any,
@@ -28,7 +29,7 @@ export class AccountDataSpaceDialogComponent {
     this.account = this.modalData["account"];
     this.dataSpaces = this.modalData["dataSpaces"];
     this.dataSpaces = JSON.parse(JSON.stringify(this.dataSpaces)); //deep clone, override immutable obj
-    this.dataSpaceOptions$ = this.cachedAccountService.getDataSpaceConfig();
+    this.initDSTypeSelectOptions();
   }
 
   onCancel(): void {
@@ -45,5 +46,13 @@ export class AccountDataSpaceDialogComponent {
         attributes: [{id:0, name: '', value: '', valueAsString: '', type: null}]
       }
     ];
+    this.initDSTypeSelectOptions();
+  }
+
+  private initDSTypeSelectOptions() {
+    this.dataSpaceOptions$ = this.cachedAccountService.getDataSpaceConfig().pipe(
+      map((dataSpaceOptions) => dataSpaceOptions.filter((dsOption) => !this.dataSpaces.find((ds) => ds.type == dsOption.value))),
+      tap((dataSpaceOptions) => this.canAddDataSpace = dataSpaceOptions.length > 0 )
+    );
   }
 }
